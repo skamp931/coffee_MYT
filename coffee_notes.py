49952d47b3dd06ae3279956dd306e430b9c1feb8
skamp931 <43207451+skamp931@users.noreply.github.com>
2025-02-11 22:35:18 +0900

    Update coffee_notes.py

--- coffee_notes.py
+++ coffee_notes.py
@@ -22,34 +22,3 @@ def load_diary():
 # 日記データを保存する関数
 def save_diary(diary_df):
     diary_df.to_csv(DIARY_FILE, index=False)
-
-# 日記を保存するボタン
-if st.button("日記を保存"):
-    if store_name or impressions:
-        diary_df = load_diary()
-        photo_filename = None
-        if photo:
-            photo_filename = os.path.join(PHOTO_DIR, f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{photo.name}")
-            with open(photo_filename, "wb") as f:
-                f.write(photo.getbuffer())
-        
-        new_entry = pd.DataFrame([{
-            "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "store_name": store_name,
-            "store_address": store_address,
-            "coffee_info": f"焙煎度合: {roast_level}, 豆の挽き方: {grind_type}",
-            "taste_characteristics": f"香り: {aroma}, 酸味: {acidity}, 甘味: {sweetness}, コク: {body}, 後味: {aftertaste}",
-            "impressions": impressions,
-            "photo": photo_filename
-        }])
-        
-        diary_df = pd.concat([diary_df, new_entry], ignore_index=True)
-        save_diary(diary_df)
-        st.success("日記が保存されました。")
-    else:
-        st.error("少なくとも1つの項目を入力してください。")
-
-# 保存された日記を表示
-st.write("保存された日記:")
-diary_df = load_diary()
-st.dataframe(diary_df)
